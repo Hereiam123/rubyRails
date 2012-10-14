daad687c0e2b90d29d17051461f57720faefbd96
Brian <bdemaio1@gmail.com>
2012-10-14 15:13:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'bf60a63de3f1d98254e74b7d8d92c85dd9a87e46dbaa105bed7bbaf22a648a0ff9ce0dc67f9547fca9630ad0beef6a6ad93a4a8ec23d3f6d50dcfe545f521edb'
---
+++ test/unit/helpers/wecome_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class WecomeHelperTest < ActionView::TestCase
+end
